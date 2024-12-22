5070699502190f431aaf7e2428241675d2c30a9d
Dr. Mohammed Qasim <87497268+MohammedTsmu@users.noreply.github.com>
2024-12-22 16:28:24 +0300

    ui+

--- Accountant/Forms/SettingsForm.Designer.cs
+++ Accountant/Forms/SettingsForm.Designer.cs
@@ -45,9 +45,9 @@
             this.btnBackup.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnBackup.Appearance.Font = new System.Drawing.Font("LBC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBackup.Appearance.Options.UseFont = true;
-            this.btnBackup.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnPrintReport.ImageOptions.SvgImage")));
-            this.btnBackup.Location = new System.Drawing.Point(483, 257);
-            this.btnBackup.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnBackup.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnBackup.ImageOptions.SvgImage")));
+            this.btnBackup.Location = new System.Drawing.Point(240, 249);
+            this.btnBackup.Margin = new System.Windows.Forms.Padding(5);
             this.btnBackup.Name = "btnBackup";
             this.btnBackup.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.btnBackup.Size = new System.Drawing.Size(282, 78);
@@ -60,9 +60,9 @@
             this.btnRestore.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnRestore.Appearance.Font = new System.Drawing.Font("LBC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRestore.Appearance.Options.UseFont = true;
-            this.btnRestore.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton1.ImageOptions.SvgImage")));
-            this.btnRestore.Location = new System.Drawing.Point(483, 346);
-            this.btnRestore.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnRestore.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnRestore.ImageOptions.SvgImage")));
+            this.btnRestore.Location = new System.Drawing.Point(240, 338);
+            this.btnRestore.Margin = new System.Windows.Forms.Padding(6);
             this.btnRestore.Name = "btnRestore";
             this.btnRestore.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.btnRestore.Size = new System.Drawing.Size(282, 78);
@@ -72,8 +72,9 @@
             // 
             // chkAutoBackup
             // 
+            this.chkAutoBackup.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.chkAutoBackup.AutoSize = true;
-            this.chkAutoBackup.Location = new System.Drawing.Point(553, 452);
+            this.chkAutoBackup.Location = new System.Drawing.Point(310, 425);
             this.chkAutoBackup.Name = "chkAutoBackup";
             this.chkAutoBackup.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.chkAutoBackup.Size = new System.Drawing.Size(212, 30);
@@ -84,14 +85,16 @@
             // 
             // txtServerName
             // 
-            this.txtServerName.Location = new System.Drawing.Point(260, 67);
+            this.txtServerName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtServerName.Location = new System.Drawing.Point(17, 57);
             this.txtServerName.Name = "txtServerName";
             this.txtServerName.Size = new System.Drawing.Size(303, 33);
             this.txtServerName.TabIndex = 8;
             // 
             // txtBackupPath
             // 
-            this.txtBackupPath.Location = new System.Drawing.Point(260, 106);
+            this.txtBackupPath.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtBackupPath.Location = new System.Drawing.Point(17, 96);
             this.txtBackupPath.Name = "txtBackupPath";
             this.txtBackupPath.Size = new System.Drawing.Size(303, 33);
             this.txtBackupPath.TabIndex = 8;
@@ -101,9 +104,9 @@
             this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnSave.Appearance.Font = new System.Drawing.Font("LBC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.Appearance.Options.UseFont = true;
-            this.btnSave.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton1.ImageOptions.SvgImage")));
-            this.btnSave.Location = new System.Drawing.Point(483, 542);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.btnSave.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnSave.ImageOptions.SvgImage")));
+            this.btnSave.Location = new System.Drawing.Point(240, 600);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(8);
             this.btnSave.Name = "btnSave";
             this.btnSave.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.btnSave.Size = new System.Drawing.Size(282, 78);
@@ -116,9 +119,9 @@
             this.btnBrowseBackupPath.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnBrowseBackupPath.Appearance.Font = new System.Drawing.Font("LBC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBrowseBackupPath.Appearance.Options.UseFont = true;
-            this.btnBrowseBackupPath.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton2.ImageOptions.SvgImage")));
-            this.btnBrowseBackupPath.Location = new System.Drawing.Point(483, 164);
-            this.btnBrowseBackupPath.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.btnBrowseBackupPath.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnBrowseBackupPath.ImageOptions.SvgImage")));
+            this.btnBrowseBackupPath.Location = new System.Drawing.Point(240, 156);
+            this.btnBrowseBackupPath.Margin = new System.Windows.Forms.Padding(10);
             this.btnBrowseBackupPath.Name = "btnBrowseBackupPath";
             this.btnBrowseBackupPath.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.btnBrowseBackupPath.Size = new System.Drawing.Size(282, 78);
@@ -128,8 +131,9 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(614, 67);
+            this.label1.Location = new System.Drawing.Point(371, 57);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.label1.Size = new System.Drawing.Size(115, 26);
@@ -138,8 +142,9 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(573, 109);
+            this.label2.Location = new System.Drawing.Point(330, 99);
             this.label2.Name = "label2";
             this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.label2.Size = new System.Drawing.Size(197, 26);
@@ -150,7 +155,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 661);
+            this.ClientSize = new System.Drawing.Size(536, 695);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtBackupPath);
@@ -168,7 +173,7 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SettingsForm";
+            this.Text = "الاعدادات";
             this.Load += new System.EventHandler(this.SettingsForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
